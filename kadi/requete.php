7523d944830171fca752bf3d4176a39c8dd40385
macbookair <kadidiatoudiall78@gmail.com>
2020-03-25 19:17:58 +0000

    modifier une activite
    Signed-off-by: macbookair <kadidiatoudiall78@gmail.com>

--- kadi/requete.php
+++ kadi/requete.php
@@ -9,9 +9,36 @@ function ajout($nom, $date, $lieu, $objet)
 
 }
 
-function afficherListe()
-{
-    global $db;
-    $req = "SELECT * FROM activite  ";
-    return $db->query($req)->fetchAll();
-}
+    function afficherListe()
+    {
+        global $db;
+        $req = "SELECT * FROM activite  ";
+        return $db->query($req)->fetchAll();
+    }
+
+    function supprimer($id)
+    {
+        global $db;
+
+        return $db->exec("DELETE FROM activite WHERE idactivite = $id");
+    }
+    function modifier($id, $nom, $lieu, $objet ,$datea)
+    {
+        global $db;
+        $req = "UPDATE activite SET nom = '$nom', datea = '$datea', lieu = '$lieu', objet ='$objet' WHERE idactivite = '$id'";
+        return $db->exec($req);
+    }
+
+    function findActiviteById($id)
+    {
+        global $db;
+        $req = "SELECT * FROM activite WHERE idactivite= $id";
+        return $db->query($req)->fetch();
+    }
+    function getActivite($id)
+    {
+        global $db;
+        $req = "SELECT * FROM activite WHERE idactivite = '$id'";
+        return $db->query($req)->fetch();
+    }
+
